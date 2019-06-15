69c27bc7b23cb9e403897dbf17aa507fc0ff1cde
Samuel Ramon <samuelrbo@gmail.com>
2019-06-14 22:14:04 -0300

    bootstraping application

---
+++ src/main/java/br/com/samuelramon/estudospringboot/EstudoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.samuelramon.estudospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringBootApplication.class, args);
+	}
+
+}
